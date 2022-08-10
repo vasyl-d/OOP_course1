e3c7e3c8f66d43181b5cbe8a9ef7a86b23a77e39
vasyl-d <vpets07@gmail.com>
2022-08-10 18:29:27 +0300

    add flask counter

---
+++ flask_counter/flask_add.py
@@ -0,0 +1,40 @@
+from flask import Flask, session, request, Response
+
+app = Flask(__name__)
+app.secret_key = 'jump$in_secret!_value'
+svg_pixel = '''<?xml version="1.0" encoding="utf-8"?>
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+                width="1px" height="1px" viewBox='0 0 1 1' style="border:0;">
+                <line/>
+            </svg>'''
+
+svg_str = '''<?xml version="1.0" encoding="utf-8"?>
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+    width="{1}" height="14"><text font-size="12" y="1em" font-family="Helvetica">{0}</text></svg> '''
+
+
+@app.route('/<app>', methods=['GET'])
+def index(app):
+    if app =='add':
+        count_str = request.args.get('str')
+        #add string sanitaze
+        num = float(count_str) if count_str else 0
+        if 'counter' in session:
+            session['counter'] += num
+        else:
+            session['counter'] = num
+        return Response(svg_pixel, mimetype='image/svg+xml;charset=UTF-8')
+    else:
+        if 'counter' in session:
+            text_output = str(session['counter'])
+      
+            session.pop('counter')
+            l = len(text_output)*18
+            svg = svg_str.format(text_output, str(l))
+            return Response(svg, mimetype='image/svg+xml;charset=UTF-8')
+    return Response(svg_pixel, mimetype='image/svg+xml;charset=UTF-8')
+
+
+if __name__ == '__main__':
+    app.debug = False
+    app.run()
